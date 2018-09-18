934d342f550f4b93c1ef0923baaff1b267c74a6d
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-09-18 11:15:21 +0200

    .

--- Models/DataProcessing/clean.R
+++ Models/DataProcessing/clean.R
@@ -5,20 +5,39 @@ targetDir<- paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/clean/');dir.create(
 
 cityfiles = list('FR'='France','BR'='Brazil','CN'='China','IN'='India','RU'='Russia','ZA'='South-Africa','US'='USA')
 
+#' TODO : option to remove small or not
+
 # france : data ok
-countrycode = 'FR'
-cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
-write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
+#countrycode = 'FR'
+#cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
+#write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
+# work on Europe instead
+
+# europe
+countrycode = 'EU'
+cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/tradeve/agglo.csv'),sep=";",dec = ",")
+cities = cities[,c("Unit_Code_Ori","Name","X_WGS","Y_WGS","Pop_1961","Pop_1971","Pop_1981","Pop_1991","Pop_2001","Pop_2011")]
+colnames(cities)<-c("ID","Name","Long","Lat","X1961","X1971","X1981","X1991","X2001","X2011")
+# dim = 3982   10
+#cities[apply(cities,1,function(r){length(which(is.na(r)))>0}),]
+# remove nas (20 rows)
+cities=cities[apply(cities,1,function(r){length(which(is.na(r)))==0}),]
+cities = cities[cities$X2011>=median(cities$X2011),]
+write.table(cities,file=paste0(targetDir,'Europe.csv'),row.names = F,sep=",")
+
 
 # china : idem
+# for china do not remove below medium-sized as already well aggregated, no small areas in the db
 countrycode = 'CN'
 cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 # brazil : remove altitude
+# and small areas
 countrycode = 'BR'
 cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
 cities = cities[,c("ID","Name","Long","Lat","X1960","X1970","X1980","X1991","X2000","X2010")]
+cities = cities[cities$X2010>=median(cities$X2010),]
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 
@@ -27,6 +46,7 @@ countrycode = 'IN'
 cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
 cities = cities[,c("ID","Name","Long","Lat","X1961","X1981","X1991","X2001","X2011")]
 cities=cities[apply(cities[,c("X1961","X1981","X1991","X2001","X2011")],1,function(r){length(which(is.na(r)))==0}),]
+cities = cities[cities$X2011>=median(cities$X2011),]
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 
@@ -40,27 +60,32 @@ cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/
 cities = cities[,c("ID","Name","Long","Lat","X1959","X1970","X1979","X1989","X2002","X2010")]
 for(j in 5:ncol(cities)){cities[,j]=as.numeric(as.character(cities[,j]))}
 cities[cities==0.0]=0.1
+cities = cities[cities$X2010>=median(cities$X2010),]
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 # South Africa
 countrycode='ZA'
 cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
 # remove nas and <10000 in 2001
-cities=cities[!is.na(cities$X2001),];cities=cities[cities$X2001>10000,]
+cities=cities[!is.na(cities$X2001),];
+#cities=cities[cities$X2001>10000,]
 # keep after 1960 and put negligible values instead of nas (Bantoustans)
 cities = cities[,c("ID","Name","Long","Lat","X1960","X1970","X1980","X1991","X1996","X2001")]
 cities[is.na(cities)]=0.1
+cities = cities[cities$X2001>=median(cities$X2001),]
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 # US
 countrycode='US'
 cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/Geodivercity/data/',cityfiles[[countrycode]],'.csv'))
 # idem south africa
-cities=cities[!is.na(cities$X2010),];cities=cities[cities$X2010>10000,]
+cities=cities[!is.na(cities$X2010),];
+#cities=cities[cities$X2010>10000,]
 # keep after 1960 (should do non stationary version of the model otherwise)
 # -> model with appearing cities ? much more complicated..
 cities = cities[,c("ID","Name","Long","Lat","X1960","X1970","X1980","X1990","X2000","X2010")]
 cities[is.na(cities)]=0.1
+cities = cities[cities$X2010>=median(cities$X2010),]
 write.table(cities,file=paste0(targetDir,cityfiles[[countrycode]],'.csv'),row.names = F,sep=",")
 
 
--- Models/DataProcessing/distMats.R
+++ Models/DataProcessing/distMats.R
@@ -9,42 +9,48 @@ source('functions.R')
 
 targetDir <- paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/processed/');dir.create(targetDir)
 
-#countries <- st_read(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/gis'),'countries')
-countries <- readOGR(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/gis'),'countries')
-countries = spTransform(countries,"+proj=longlat +ellps=clrk66 +no_defs")
+countrycodes = c('EU','BR','CN','IN','RU','ZA','US')
+# 'FR'='France'
+cityfiles = list('EU'='Europe','BR'='Brazil','CN'='China','IN'='India','RU'='Russia','ZA'='South-Africa','US'='USA')
 
-dem <- raster(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/DEM/raw/DEM_geotiff/alwdgg.tif'))
+withGraph = F
 
-#country = as_Spatial(countries[countries$CNTR_ID=='FR',])
-
-#countrycodes = c('FR','BR','CN','IN','RU','ZA','US')
-#countrycodes = c('CN','IN','RU','ZA','US')
-countrycodes=c('ZA','US')
-cityfiles = list('FR'='France','BR'='Brazil','CN'='China','IN'='India','RU'='Russia','ZA'='South-Africa','US'='USA')
+if(withGraph){
+  dem <- raster(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/DEM/raw/DEM_geotiff/alwdgg.tif'))
+  #countries <- st_read(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/gis'),'countries')
+  countries <- readOGR(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/gis'),'countries')
+  countries = spTransform(countries,"+proj=longlat +ellps=clrk66 +no_defs")
+  #country = as_Spatial(countries[countries$CNTR_ID=='FR',])
+}
 
 for(countrycode in countrycodes){
   show(paste0('Constructing for country ',countrycode))
-  extrdem = extractRaster(dem,countries,countrycode)
-  graph = geographicGraph(extrdem)
+  
   cities <- read.csv(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Data/clean/',cityfiles[[countrycode]],'.csv'))
   
-  vertices = apply(cities[,c("Long","Lat")],1,function(r){
-    d=(V(graph)$x-r[1])^2+(V(graph)$y-r[2])^2
-    #show(min(d))
-    return(which(d==min(d))[1])
-  })
-  cities$vertices=vertices
-  
-  cities <- mergeDuplicatedCities(cities)
-  
-  slopes = atan(abs(E(graph)$slope)/(E(graph)$length*1000))*360/(2*pi)
-  
-  #alpha0 = 3;n0 = 3
-  for(alpha0 in 2:4){
-    for(n0 in 2:4){
-      impedances = E(graph)$length*(1 + (slopes/alpha0)^n0)
-      graphdists = distances(graph,v = cities$vertices,to=cities$vertices,weights = impedances)
-      write.table(graphdists,file=paste0(targetDir,countrycode,'_gdist_alpha0',alpha0,'_n0',n0,'.csv'),row.names = F,col.names = F,sep=',')
+  if(withGraph){
+    extrdem = extractRaster(dem,countries,countrycode)
+    graph = geographicGraph(extrdem)
+    save(graph,file=paste0(targetDir,countrycode,'_graph.RData'))
+    
+    
+    vertices = apply(cities[,c("Long","Lat")],1,function(r){
+      d=(V(graph)$x-r[1])^2+(V(graph)$y-r[2])^2
+      #show(min(d))
+      return(which(d==min(d))[1])
+    })
+    cities$vertices=vertices
+    cities <- mergeDuplicatedCities(cities)
+    
+    slopes = atan(abs(E(graph)$slope)/(E(graph)$length*1000))*360/(2*pi)
+    
+    #alpha0 = 3;n0 = 3
+    for(alpha0 in 2:4){
+      for(n0 in 2:4){
+        impedances = E(graph)$length*(1 + (slopes/alpha0)^n0)
+        graphdists = distances(graph,v = cities$vertices,to=cities$vertices,weights = impedances)
+        write.table(graphdists,file=paste0(targetDir,countrycode,'_gdist_alpha0',alpha0,'_n0',n0,'.csv'),row.names = F,col.names = F,sep=',')
+      }
     }
   }
   
