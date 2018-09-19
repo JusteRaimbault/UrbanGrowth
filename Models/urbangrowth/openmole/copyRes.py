f9d53f8fca1718684d2e18b4ff0cbeab516b563e
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-09-19 10:56:08 +0200

    res

--- Models/urbangrowth/openmole/copyRes.py
+++ Models/urbangrowth/openmole/copyRes.py
@@ -7,7 +7,8 @@ TARGET=sys.argv[2]
 
 def getLatestGen(dir):
     files = os.listdir(dir)
-    return(map(lambda s : int(s.split('population')[1].split('.')[0]),files).sort(reverse=T)[0])
+    print(sorted(map(lambda s:int(s.split('population')[1].split('.')[0]),files),reverse=True)[0])
+    return(str(sorted(map(lambda s : int(s.split('population')[1].split('.')[0]),files),reverse=True)[0]))
 
 
 sourcedirs = os.listdir(SOURCE)
@@ -16,6 +17,6 @@ targetdirs = os.listdir(TARGET)
 for dir in sourcedirs :
     if not os.path.isdir(TARGET+'/'+dir):
         os.mkdir(TARGET+'/'+dir)
-    gen = getLatestGen(dir)
+    gen = getLatestGen(SOURCE+'/'+dir)
     if not os.path.isfile(TARGET+'/'+dir+'/population'+gen+'.csv'):
         shutil.copy(SOURCE+'/'+dir+'/population'+gen+'.csv',TARGET+'/'+dir+'/population'+gen+'.csv')
