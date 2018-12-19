88dbc3dddc85da253d385ca02bc7113b597d5d2d
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-12-19 14:36:55 +0100

    .

--- Models/Analysis/convergence.R
+++ Models/Analysis/convergence.R
@@ -62,14 +62,21 @@ ggsave(file=paste0(resdir,'volumes_nobounds.png'),width=15,height=10,units='cm')
 oldcalib = 'CALIB_intgib_BR_20180921_173302'
 #'CALIB_intgib_BR_20181003_154646/population42744.csv'
 #'CALIB_intgib_BR_20180921_173302/population39261.csv'
-local = cbind(read.csv(file=paste0(oldcalib,'/population39261.csv')),type='local')
+oldcalib2='CALIB_intgib_BR_20181218_131615'
 
-d = rbind(cbind(pop96000,type='grid'),)
+local = cbind(read.csv(file=paste0(oldcalib,'/population39261.csv')),type='local')
+local2 = cbind(read.csv(file=paste0(oldcalib2,'/population34000.csv')),type='local2')
+
+d = rbind(cbind(pop96000,type='grid'),local)
 g=ggplot(d,aes(x=logmse,y=mselog,color=type))
 g+geom_point()
 # -> what the fuck ? check num of cities
 
+g=ggplot(local,aes(x=logmse,y=mselog,color=type))
+g+geom_point()
 
+g=ggplot(rbind(local,local2),aes(x=logmse,y=mselog,color=type))
+g+geom_point()
 
 
 
