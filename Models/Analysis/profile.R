92d1d248ecd4eac1b2610b2a3c18c2bb44a06121
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-02-12 11:36:21 +0100

    skeleton_abstract

--- Models/Analysis/profile.R
+++ Models/Analysis/profile.R
@@ -13,12 +13,13 @@ sourcedir = 'PROFILE_GRID_intgib_BR_20181221_103649/'
 resdir = paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Results/Calibration/',sourcedir);dir.create(resdir)
 
 
-res=as.tbl(read.csv(file=paste0(sourcedir,'population6899.csv')))
+#res=as.tbl(read.csv(file=paste0(sourcedir,'population6899.csv')))
+res=as.tbl(read.csv(file=paste0(sourcedir,'population20000.csv')))
 
 # g=ggplot(res[res$gravityDecay<=1000,],aes(x=gravityDecay,y=logmse))
 g=ggplot(res,aes(x=gravityDecay,y=logmse))
 g+geom_point()+geom_line()+stdtheme
-ggsave(file=paste0(resdir,'profile_logmse-gravityDecay.png'),width=15,height = 10,units='cm')
+ggsave(file=paste0(resdir,'profile_logmse-gravityDecay_gen20000.png'),width=15,height = 10,units='cm')
 
 # Q : 
 #  - more precise profile in the [0,100] interval ? -> relaunch more precise
