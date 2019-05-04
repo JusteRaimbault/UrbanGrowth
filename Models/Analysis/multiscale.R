e701df1d93ebc70a9c8ee3a8c7e82cf45c156257
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-04 22:38:07 +0200

    res_figs

--- Models/Analysis/multiscale.R
+++ Models/Analysis/multiscale.R
@@ -9,7 +9,7 @@ source(paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Models/Analysis/functions.R'))
 
 
 resPrefix = '20190429_193158_MULTISCALE_GRID_GRID'
-resdir = paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Results/Multiscale/',resPrefix);dir.create(resdir)
+resdir = paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Results/Multiscale/',resPrefix,'/');dir.create(resdir)
 
 res <- as.tbl(read.csv(paste0('exploration/',resPrefix,'.csv')))
 
@@ -51,12 +51,18 @@ sres=sres[sres$macroGrowthRate==0.0&sres$macroMesoAlphaUpdateMax==0.1&sres$macro
 
 
 g=ggplot(sres,aes(x=macroInteractionDecay,y=deltaHierarchymacroAccessibilities,group=mesoTimeSteps,color=mesoTimeSteps))
-g+geom_line()+facet_grid(mesoMacroDecayUpdateMax~macroMesoBetaUpdateMax)
+g+geom_line()+facet_grid(mesoMacroDecayUpdateMax~macroMesoBetaUpdateMax)+stdtheme
+ggsave(file=paste0(resdir,'deltaHierarchyAccessibility.png'),width=20,height=18,units='cm')
 
 g=ggplot(sres,aes(x=macroInteractionDecay,y=deltamesoMorans,group=mesoTimeSteps,color=mesoTimeSteps))
 g+geom_line()+facet_grid(mesoMacroDecayUpdateMax~macroMesoBetaUpdateMax,scales="free")
+ggsave(file=paste0(resdir,'deltaMoran.png'),width=20,height=18,units='cm')
 
 g=ggplot(sres,aes(x=macroInteractionDecay,y=deltamesoDistances,group=mesoTimeSteps,color=mesoTimeSteps))
 g+geom_line()+facet_grid(mesoMacroDecayUpdateMax~macroMesoBetaUpdateMax,scales="free")
+ggsave(file=paste0(resdir,'deltaDistances.png'),width=20,height=18,units='cm')
+
+
+
 
 
