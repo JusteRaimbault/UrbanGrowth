211c03af11fc3d76c6afd4cd3f894a9bfa397382
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-09-26 18:03:04 +0100

    prepare_for_proper_submodule

--- Models/urbangrowth/project/plugins.sbt
+++
@@ -1,1 +0,0 @@
-addSbtPlugin("com.typesafe.sbt" % "sbt-osgi" % "0.9.2")
--- Models/urbangrowth/src/main/scala/urbangrowth/models/marius/DefaultValues.scala
+++
@@ -1,9 +0,0 @@
-
-package urbangrowth.models.marius
-
-/** Default values for some parameters of the models */
-trait DefaultValues {
-  /** Simulate from 1959 to 1989 */
-  def maxStep = 30
-}
-
