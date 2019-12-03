4a1415a2220d3e5476a3580aca995f36dd107ef6
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-12-03 10:51:58 +0100

    200-250

--- Models/MesoCalibration/runRangeYear.sh
+++ Models/MesoCalibration/runRangeYear.sh
@@ -1,1 +1,1 @@
-cat $1 | awk -F"," '{print "rm Calib.oms;echo "$2" ; echo \"val (areaind,endyear) = ("$1","$2")\" >> Calib.oms; cat MesoCalibrationYear.oms >> Calib.oms; openmole -p density_2.12-1.0.jar --script Calib.oms --password-file omlpsswd"}' | sh
+cat $1 | awk -F"," '{print "rm Calib.oms;echo "$2" ; echo \"val (areaind,endyear) = ("$1","$2")\" >> Calib.oms; cat MesoCalibrationYear.oms >> Calib.oms; /home/juste/openmole-9.2/openmole -p density_2.12-1.0.jar --script Calib.oms --password-file omlpsswd"}' | sh
