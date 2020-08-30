0497a638e71059420a78bfe300db3d14b6add2c3
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-08-30 15:04:39 +0100

    .

--- Docs/Papers/MesoBenchmark/paper/figures.sh
+++ Docs/Papers/MesoBenchmark/paper/figures.sh
@@ -10,3 +10,9 @@ montage figuresraw/mapindic_alpha_2015.png figuresraw/mapindic_moran_2015.png -t
 
 # Fig 2
 montage figuresraw/mapindic_cluster.png figuresraw/distributions_cluster-year.png -tile 1x2 -geometry +0+"$VERTICALPADDING" -resize "$WIDTH"x figures/Fig2.png
+
+# Fig 3
+montage figuresraw/CorrelatedPerco_size200_gradient002_corr04_centers3_nonBinary.png figuresraw/Expmixture_size200_centers10_maxRadiusRate0-2_hierarchy1_seed-2489151330471457438.png figuresraw/Gravity_size200_g03_gravity2-5_hierarchy0-5_centers3_pop20000_seed7990907742188865088.png figuresraw/ReactionDiffusion_size200_alpha0-8_beta0-2_diffSteps1_growthRate100_population5000_seed-2225875956205852410.png -tile 2x2 -geometry +"$HORIZONTALPADDING"+"$VERTICALPADDING" -resize "$WIDTH"x -border 2 -bordercolor Black figures/Fig3.png
+
+# Fig 4
+cp figuresraw/scatter_all.png figures/Fig4.png
--- Models/MesoBenchmark/pseResults.R
+++ Models/MesoBenchmark/pseResults.R
@@ -5,21 +5,78 @@ library(ggplot2)
 library(dplyr)
 library(GGally)
 
+source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
+
+resdir = paste0(Sys.getenv('CS_HOME'),'/UrbanGrowth/Results/MesoBenchmark/')
 
 files = list(
-  'expmixture' = '20200828_1515_PSE_EXPMIXTURE_res/population20000.csv',
-  'reactiondiffusion' = '20200828_1624_PSE_REACTIONDIFFUSION_res/population50000.csv'
+  'expmixture' = 'openmole/pse/20200829_1514_PSE_EXPMIXTURE_res/population100000.csv',
+  'reactiondiffusion' = 'openmole/pse/20200828_1624_PSE_REACTIONDIFFUSION_res/population50000.csv',
+  'correlatedperco' = 'openmole/pse/20200829_1330_PSE_CORRELATEDPERCO_res/population100000.csv',
+  'gravity' = 'openmole/pse/20200829_1526_PSE_GRAVITY_res/population37000.csv'
 )
 
 indics = c('moran','distance','entropy','slope')
 
 res = data.frame()
 for(model in names(files)){
-  currentres = as.tbl(read.csv(paste0('openmole/pse/',files[[model]])))
+  currentres = as.tbl(read.csv(files[[model]]))
   res = rbind(res,cbind(currentres[,c(indics,'evolution.samples')],model=rep(model,nrow(currentres))))
 }
 
-ggpairs(res[res$slope>-4,indics],aes(color=model))
+
+ggsave(plot = ggpairs(res[res$slope>-4,],aes(color=model),columns = indics,
+                      lower = list(continuous = wrap("points", alpha = 0.6,size=0.2,shape='.')),
+                      diag = list(continuous = wrap("densityDiag", alpha = 0.4))
+                      )+stdtheme,filename = paste0(resdir,'scatter_models.png'),width = 40,height=30,units='cm')
 
 
+# with real points
+morphos <- as.tbl(read.csv(file='../MesoCalibration/configs/morphologies.csv',header = T,sep = ";"))
+real = morphos[,c("moran2015","avgDist2015","entropy2015","alpha2015")]
+names(real)<- indics
+
+res = rbind(res,cbind(real,evolution.samples=rep(0,nrow(real)),model=rep('real',nrow(real))))
+
+ggsave(plot = ggpairs(res[res$slope>-4,],aes(color=model),columns = indics,
+                      lower = list(continuous = wrap("points", alpha = 0.7,size=0.15,shape='.')),
+                      diag = list(continuous = wrap("densityDiag", alpha = 0.4))
+)+stdtheme,filename = paste0(resdir,'scatter_all.png'),width = 40,height=30,units='cm')
+
+
+
+####
 # for hypervolume intersection: https://rdrr.io/cran/hypervolume/man/hypervolume_set.html
+
+library(hypervolume)
+
+models = c(names(files),'real')
+res=res[res$slope>-4,]
+
+hvs = list()
+for(model in models){
+  show(model)
+  start = as.numeric(Sys.time())
+  hvs[[model]] = hypervolume_gaussian(res[res$model==model,indics])
+  show(as.numeric(Sys.time()) - start)
+}
+sapply(hvs,function(hv){hv@Volume})
+
+overlaps = matrix(data=rep(1,length(models)*length(models)),nrow=length(models))
+for(model1 in 1:length(models)){
+  for (model2 in 1:length(models)){
+    if(model1!=model2){
+      show(paste0(models[model1],' / ',models[model2]))
+      hvset = hypervolume_set(hvs[[models[model1]]],hvs[[models[model2]]],check.memory=FALSE)
+      overlaps[model1,model2] = hvset@HVList$Intersection@Volume / hvs[[models[model2]]]@Volume
+    }
+  }
+}
+rownames(overlaps)<-models
+colnames(overlaps)<-models
+
+melt(overlaps)
+
+
+
+
